6ddbbad56acb878c8cfe5f49c656a9d0bb633ff2
Shaik Ishaq <sonuishaq67@gmail.com>
2023-05-08 18:29:01 +0530

    added api

--- newApp/server/server.js
+++ newApp/server/server.js
@@ -1,8 +1,48 @@
 const express = require('express')
+const { exec } = require("child_process");
 const app = express()
 
-app.get('/api',(req,res)=>{
-    res.json({'users':['userOne',"userTwo"]})
+app.get('/lof',(req,res)=>{
+    let ret=""
+    exec("cd data && ls", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log("ishaq",stdout)
+        ret=stdout
+        ret=ret.split('\n')
+        ret.splice(ret.length-1,1)
+        res.json(ret)
+    });
 })
 
-app.listen(5002,()=>{console.log('server start')})
+app.get('/device/:id',(req,res)=>{
+    exec(`cd data/${req.params.id} && cat distance.txt && echo "" && cat humidity.txt && echo "" && cat temperature.txt && echo ""`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log("ishaq",stdout)
+        ret=stdout
+        ret=ret.split('\n')
+        ret.splice(ret.length-1,1)
+        
+        retdata={
+            "distance":ret[0],
+            "humidity":ret[1],
+            "temperature":ret[2]
+        }
+        res.json(retdata)
+    });
+})
+
+app.listen(5002,()=>{console.log('server start port 5002')})
